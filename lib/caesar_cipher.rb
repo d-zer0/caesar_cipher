05b5759667712ddb282e7e7f5becafc13f4d843d
Daniel Varcas <daniel.varcas@gmail.com>
2016-04-28 22:02:47 +0100

    added fix to pass large number test. All tests pass.

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,36 +1,53 @@
-#def initialize
-#	i = 0
-#	shift = 5
-#end
-
 def encrypt (text, shift)
 
+	# gets remainder of shift after all full loops through alphabet considered
 	shift = (shift % 26)
+	
+	# counter
 	i = 0
+
+	# splits input text into an array of it's characters
 	text = text.split(//)
 
+	# itterates through each character in array
 	text.each do
 
-		t = text[i].ord
-		if (t >= 65 && t <= 90)
+		# assigns value of text[i] to x
+		x = text[i].ord
+
+		# checks if x is an upper-case letter
+		if (x >= 65 && x <= 90)
 			letter = "upcase"
-		elsif (t >= 97 && t <= 122)
+
+		# checks if x is a lower-case letter
+		elsif (x >= 97 && x <= 122)
 			letter = "downcase"
 		end
 
-		if (letter == "upcase") || (letter == "downcase") # check if character is a letter
-			text[i] = text[i].ord # convert char to ascii value
-			
+		# only accepts letters
+		if (letter == "upcase") || (letter == "downcase")
+			text[i] = text[i].ord # convert char to ascii value		
 
-			if (letter == "upcase") && ((text[i] + shift) > 90) || (letter == "downcase") && ((text[i] + shift) > 122) # check shift keeps char within case range
-				shift -= 26 # find remainder of shift after each full loop through alphabet
-			end
-			
+			# stops char shifting beyond 'Z' or 'z'
+			if (letter == "upcase") && ((text[i] + shift) > 90) || (letter == "downcase") && ((text[i] + shift) > 122)
+				# find remainder of shift after each full loop through alphabet
+				shift = (shift - 26)
 
-			text[i] = (text[i] + shift).chr # change ascii value, change back to char
+			# stops char shifting backwards beyond 'A' or 'A'
+			elsif (letter == "upcase") && ((text[i] + shift) < 65) || (letter == "downcase") && ((text[i] + shift) < 97)
+				# find remainder of shift after each full loop through alphabet
+				shift = (shift + 26)
+			end	
+
+			# changes ascii value by value of shift and converts back to char
+			text[i] = (text[i] + shift).chr
+		
 		end
+
+		# adds 1 to count
 		i += 1
-	end.join('')
+
+	end.join('') # converts array back to string
 end
 
 =begin
