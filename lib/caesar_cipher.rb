4302ece60aa21342721f6aeff7659cb8dd2ab885
Daniel Varcas <daniel.varcas@gmail.com>
2016-04-28 19:50:10 +0100

    added tests, passes 5 of 6

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,3 +1,20 @@
-def add (x, y)
-	x + y
+#def initialize
+#	i = 0
+#	shift = 5
+#end
+
+def encrypt (text, shift)
+
+	i = 0
+	text = text.split(//)
+
+	text.each do
+
+		t = text[i].ord
+		if (t >= 65 && t <= 90) || (t >= 97 && t <= 122)
+			text[i] = text[i].ord
+			text[i] = (text[i] + shift).chr
+		end
+		i += 1
+	end.join('')
 end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,7 +1,29 @@
 require 'caesar_cipher'
 
-describe 'add' do
-	it 'adds two passed numbers' do
-		expect(add(1, 2)).to eq(3)
+describe 'encrypt' do
+
+	it 'shifts a letter down the alphabet by a given number' do
+		expect(encrypt('a', 2)).to eq('c')
 	end
+
+	it 'encrypts a whole word' do
+		expect(encrypt('abc', 3)).to eq('def')
+	end
+
+	it 'retains whitespace' do
+		expect(encrypt('a ', 2)).to eq('c ')
+	end
+
+	it 'retains special characters and numbers' do
+		expect(encrypt('abc?!# 123', 3)).to eq('def?!# 123')
+	end
+
+	it 'encrypts multiple words' do
+		expect(encrypt('aaa bbb ccc', 1)).to eq('bbb ccc ddd')
+	end
+
+	it 'shifts by a large number' do
+		expect(encrypt('abc de fg', 2601)).to eq('bcd ef gh')
+	end
+
 end
